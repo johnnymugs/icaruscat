e8ad1181b81459697237599925a0eb0067ed00c8
Jonathan Mukai-Heidt <johnnymukai@gmail.com>
2012-12-23 14:03:15 -0500

    rudimentary pass at combining keyhandling & sprite

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,44 @@
-var game = { };
+// clean this up #TODO
+var lastFrameTime = new Date().getTime();
+
+game = {
+  run: function run(){
+    var now = new Date().getTime(), dt = now - (lastFrameTime || now);
+    if (dt >= 30) {
+      var ctx = document.getElementById('game').getContext("2d");
+      ctx.fillStyle = "rgb(100,100,100)";
+      ctx.fillRect (0, 0, 960, 500);
+
+      for (var i in keyHandler.queue){
+        var el = keyHandler.queue[i];
+        drawSprite(ctx, 'regChars', el.keyCode, el.x, el.y, 32, 32);
+      }
+
+      var nextAry = [];
+      for (var i in actors){
+        var el = actors[i];
+        drawSprite(ctx, 'explosion', el.currentFrame, el.x - 40, el.y - 40);
+        if (el.currentFrame === 26){
+        }else{
+          el.currentFrame++;
+          nextAry.push(el);
+        }
+      }
+      actors = nextAry;
+
+      lastFrameTime = now;
+    }
+    requestAnimationFrame(run);
+  }
+}
+
+// move this polyfill out somewhere
+   var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                                 window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+                                   window.requestAnimationFrame = requestAnimationFrame;
+
+window.onload = function(){
+  game.run();
+  keyHandler.loadQueue("abcabcabc");
+  window.onkeyup = keyHandler.handlePress;
+};
